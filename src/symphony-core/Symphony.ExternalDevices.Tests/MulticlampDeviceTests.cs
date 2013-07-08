48e6ec55e46e3732d17ef9108084d6e8c0ede15b
Mark Cafaro <mark.cafaro@gmail.com>
2013-07-08 09:39:48 -0700

    Updated multiclamp tests for IsRunning

--- src/symphony-core/Symphony.ExternalDevices.Tests/MulticlampDeviceTests.cs
+++ src/symphony-core/Symphony.ExternalDevices.Tests/MulticlampDeviceTests.cs
@@ -701,7 +701,7 @@ namespace Symphony.ExternalDevices
             var mcd = new MultiClampDevice(mc, c, background);
             mcd.BindStream(s);
 
-            daq.ExpectAndReturn("get_Running", false);
+            daq.ExpectAndReturn("get_IsRunning", false);
             daq.Expect("ApplyStreamBackground", new object[] {s});
 
             mc.FireParametersChanged(DateTimeOffset.Now, dataVClamp);
@@ -742,7 +742,7 @@ namespace Symphony.ExternalDevices
             var mcd = new MultiClampDevice(mc, c, background);
             mcd.BindStream(s);
 
-            daq.ExpectAndReturn("get_Running", true);
+            daq.ExpectAndReturn("get_IsRunning", true);
             daq.ExpectNoCall("ApplyStreamBackground");
 
             mc.FireParametersChanged(DateTimeOffset.Now, dataVClamp);
