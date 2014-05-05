7407058ac600d8a179d29198fa58d772df48f67b
Mark Cafaro <mark.cafaro@gmail.com>
2014-05-05 15:39:37 -0700

    Updated version test

--- src/symphony-core/Symphony.Core.Tests/VersionTests.cs
+++ src/symphony-core/Symphony.Core.Tests/VersionTests.cs
@@ -12,24 +12,10 @@ namespace Symphony.Core
     class VersionTests
     {
         [Test]
-        public void ShouldExposeMarketingVersion()
-        {
-            var env = Environment.GetEnvironmentVariables();
-
-            var expectedVersion = env.Contains("SYMPHONY_VERSION") ? env["SYMPHONY_VERSION"] : "0.0.0";
-
-            Assert.That(SymphonyFramework.VersionString, Is.EqualTo(expectedVersion));
-
-        }
-
-        [Test]
         public void ShouldExposeVersion()
         {
-            var env = Environment.GetEnvironmentVariables();
-            var expectedVersion = env.Contains("SYMPHONY_VERSION") ? env["SYMPHONY_VERSION"] : "0.0.0";
-
-            Assert.That(expectedVersion, Is.Not.Null);
-            Assert.That(SymphonyFramework.Version, Is.EqualTo(new Version((string) expectedVersion)));
+            var expectedVersion = typeof(Controller).Assembly.GetName().Version;
+            Assert.That(SymphonyFramework.Version, Is.EqualTo(expectedVersion));
         }
     }
 }
