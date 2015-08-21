137bfd407fec8ac0bd40d76c06379dcd850e282a
Mark Cafaro <mark.cafaro@gmail.com>
2015-08-21 11:25:42 -0700

    Improved multiclamp error message

--- src/symphony-core/Symphony.ExternalDevices/MulticlampDevice.cs
+++ src/symphony-core/Symphony.ExternalDevices/MulticlampDevice.cs
@@ -747,7 +747,7 @@ namespace Symphony.ExternalDevices
             get
             {
                 if (!HasDeviceInputParameters)
-                    throw new MultiClampDeviceException("No current device input parameters.");
+                    throw new MultiClampDeviceException("No current MultiClamp input parameters. Make sure MultiClamp Commander is open.");
 
                 return MostRecentDeviceParameterPreceedingDate(InputParameters, Clock.Now);
             }
@@ -778,7 +778,7 @@ namespace Symphony.ExternalDevices
             get
             {
                 if (!HasDeviceOutputParameters)
-                    throw new MultiClampDeviceException("No current device output parameters.");
+                    throw new MultiClampDeviceException("No current MultiClamp output parameters. Make sure MultiClamp Commander is open.");
 
                 return MostRecentDeviceParameterPreceedingDate(OutputParameters, Clock.Now);
             }
