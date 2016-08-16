bf02573afd3cd4bf35c30f67a9ac0b60e673610b
Mark Cafaro <mark.cafaro@gmail.com>
2016-08-16 16:00:15 -0700

    Added NIDAQ projects

---
+++ src/symphony-core/NIDAQController/NIDAQController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NationalInstruments.DAQmx;
+using Symphony.Core;
+
+namespace NI
+{
+    public sealed class NIDAQController : DAQControllerBase, IDisposable
+    {
+        private Device Device { get; set; }
+
+        private const string DEVICE_NAME_KEY = "DeviceName";
+
+        public string DeviceName
+        {
+            get { return (string)Configuration[DEVICE_NAME_KEY]; }
+            private set { Configuration[DEVICE_NAME_KEY] = value; }
+        }
+
+        public NIDAQController(string deviceName)
+        {
+            DeviceName = deviceName;
+            IsHardwareReady = false;
+        }
+
+        public override void BeginSetup()
+        {
+            base.BeginSetup();
+            if (!IsHardwareReady)
+            {
+                InitHardware();
+            }
+        }
+
+        public void InitHardware()
+        {
+            if (!IsHardwareReady)
+            {
+                Device = DaqSystem.Local.LoadDevice(DeviceName);
+
+                if (!DAQStreams.Any())
+                {
+                    foreach (var c in Device.AIPhysicalChannels)
+                    {
+                    }
+
+                    foreach (var c in Device.AOPhysicalChannels)
+                    {    
+                    }
+
+                    foreach (var p in Device.DIPorts)
+                    {
+                    }
+
+                    foreach (var p in Device.DOPorts)
+                    {
+                    }
+                }
+                
+                IsHardwareReady = true;
+            }
+        }
+
+        protected override void StartHardware(bool waitForTrigger)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IInputData ReadStreamAsync(IDAQInputStream s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ApplyStreamBackgroundAsync(IDAQOutputStream s, IMeasurement background)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IDictionary<IDAQInputStream, IInputData> ProcessLoopIteration(IDictionary<IDAQOutputStream, IOutputData> outData, TimeSpan deficit, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<NIDAQController> AvailableControllers()
+        {
+            return DaqSystem.Local.Devices.Select(d => new NIDAQController(d));
+        }
+    }
+}
---
+++ src/symphony-core/NIDAQController/NIDAQInputStream.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Symphony.Core;
+
+namespace NI
+{
+    class NIDAQInputStream : DAQInputStream
+    {
+        public NIDAQInputStream(string name, NIDAQController controller)
+            : base(name, controller)
+        {
+            Clock = controller.Clock;
+        }
+    }
+}
